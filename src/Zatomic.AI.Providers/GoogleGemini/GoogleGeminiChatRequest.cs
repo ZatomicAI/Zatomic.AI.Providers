db3edddecbb92211ee1e8df744ec509771b85870
Dave Donaldson <dave@arcware.net>
2025-05-20 00:39:02 -0400

    Added helpers methods to Google Gemini request object

--- src/Zatomic.AI.Providers/GoogleGemini/GoogleGeminiChatRequest.cs
+++ src/Zatomic.AI.Providers/GoogleGemini/GoogleGeminiChatRequest.cs
@@ -18,5 +18,58 @@ namespace Zatomic.AI.Providers.GoogleGemini
 		{
 			Contents = new List<GoogleGeminiChatContent>();
 		}
+
+		public GoogleGeminiChatRequest(float temperature) : this()
+		{
+			if (GenerationConfig == null)
+			{
+				GenerationConfig = new GoogleGeminiChatGenerationConfig { Temperature = temperature };
+			}
+			else
+			{
+				GenerationConfig.Temperature = temperature;
+			}
+		}
+
+		public GoogleGeminiChatRequest(float temperature, string responseMimeType) : this(temperature)
+		{
+			GenerationConfig = new GoogleGeminiChatGenerationConfig { ResponseMimeType = responseMimeType };
+		}
+
+		public void AddModelMessage(string content)
+		{
+			AddMessage("model", content);
+		}
+
+		public void AddSystemMessage(string content)
+		{
+			SystemInstruction = new GoogleGeminiChatSystemInstruction();
+			SystemInstruction.Parts.Add(new GoogleGeminiChatTextPart { Text = content });
+		}
+
+		public void AddUserMessage(string content)
+		{
+			AddMessage("user", content);
+		}
+
+		public void AddUserMessage(string content, string mimeType, string data)
+		{
+			AddMessage("user", content, mimeType, data);
+		}
+
+		private void AddMessage(string role, string content)
+		{
+			var contentObj = new GoogleGeminiChatContent { Role = role };
+			contentObj.Parts.Add(new GoogleGeminiChatTextPart { Type = "text", Text = content });
+			Contents.Add(contentObj);
+		}
+
+		private void AddMessage(string role, string content, string mimeType, string data)
+		{
+			var contentObj = new GoogleGeminiChatContent { Role = role };
+			contentObj.Parts.Add(new GoogleGeminiChatTextPart { Type = "text", Text = content });
+			contentObj.Parts.Add(new GoogleGeminiChatInlineDataPart { Type = "inlineData", MimeType = mimeType, Data = data });
+			Contents.Add(contentObj);
+		}
 	}
 }
