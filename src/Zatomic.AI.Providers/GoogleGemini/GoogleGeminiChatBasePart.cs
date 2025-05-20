dc468030066b499f0fbb6e46b32c2e644be77ecf
Dave Donaldson <dave@arcware.net>
2025-05-20 10:36:07 -0400

    Replaced pervious commit with JsonIgnore attribute, see code comment about why

--- src/Zatomic.AI.Providers/GoogleGemini/GoogleGeminiChatBasePart.cs
+++ src/Zatomic.AI.Providers/GoogleGemini/GoogleGeminiChatBasePart.cs
@@ -4,7 +4,12 @@ namespace Zatomic.AI.Providers.GoogleGemini
 {
     public abstract class GoogleGeminiChatBasePart
 	{
-		[JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+		// This property is used to identify the type of the chat part, but it's not
+		// something Google expects on input and Google doesn't returns this property
+		// on output. This is here to allow us to use GoogleGeminiChatPartsListConverter
+		// to dynamically convert the JSON to the correct type.
+
+		[JsonIgnore]
 		public string Type { get; set; }
 	}
 }
