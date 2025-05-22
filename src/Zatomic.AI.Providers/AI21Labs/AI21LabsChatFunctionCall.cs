6978038e9459e11022c932d903b12a468c493bfa
Dave Donaldson <dave@arcware.net>
2025-05-22 11:55:29 -0400

    Changed type of AI21 Labds function call arguments to JObject

--- src/Zatomic.AI.Providers/AI21Labs/AI21LabsChatFunctionCall.cs
+++ src/Zatomic.AI.Providers/AI21Labs/AI21LabsChatFunctionCall.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Zatomic.AI.Providers.AI21Labs
 {
 	public class AI21LabsChatFunctionCall
 	{
 		[JsonProperty("arguments")]
-		public object Arguments { get; set; }
+		public JObject Arguments { get; set; }
 
 		[JsonProperty("name")]
 		public string Name { get; set; }
