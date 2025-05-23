8b98fc07b8fa3bc86326b5ce2462f184026a7830
Dave Donaldson <dave@arcware.net>
2025-05-23 13:58:41 -0400

    Added support for tools for Fireworks AI

---
+++ src/Zatomic.AI.Providers/FireworksAI/FireworksAIChatToolCall.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Zatomic.AI.Providers.FireworksAI
+{
+	public class FireworksAIChatToolCall
+	{
+		[JsonProperty("function")]
+		public FireworksAIChatToolCallFunction Function { get; set; }
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("type")]
+		public string Type { get; set; }
+	}
+}
---
+++ src/Zatomic.AI.Providers/FireworksAI/FireworksAIChatToolCallFunction.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Zatomic.AI.Providers.FireworksAI
+{
+	public class FireworksAIChatToolCallFunction
+	{
+		[JsonProperty("arguments")]
+		public string Arguments { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+	}
+}
