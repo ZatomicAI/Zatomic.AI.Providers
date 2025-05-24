5e46ced04fd5f1ea77f74ec64cd5e284287b18a4
Dave Donaldson <dave@arcware.net>
2025-05-24 13:58:55 -0400

    Support for most of the Meta tools

---
+++ src/Zatomic.AI.Providers/Meta/MetaChatTool.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Zatomic.AI.Providers.Meta
+{
+	public class MetaChatTool
+	{
+		[JsonProperty("function")]
+		public MetaChatToolFunction Function { get; set; }
+
+		[JsonProperty("type")]
+		public string Type { get; set; }
+	}
+}
